e61a61da596d9c814f758f33c4300fd321137db5
kcrg <kacper@tryniecki.com>
2019-07-01 02:16:06 +0200

    Preparing for XAML Islands

--- NotatnikMechanika.Forms/NotatnikMechanika.Forms.Android/MainActivity.cs
+++ NotatnikMechanika.Forms/NotatnikMechanika.Forms.Android/MainActivity.cs
@@ -1,5 +1,4 @@
-﻿
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
@@ -7,7 +6,7 @@ using Android.Runtime;
 namespace NotatnikMechanika.Forms.Droid
 {
     [Activity(Label = "NotatnikMechanika.Forms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -16,14 +15,11 @@ namespace NotatnikMechanika.Forms.Droid
 
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
--- NotatnikMechanika.Forms/NotatnikMechanika.Forms/MainPage.xaml.cs
+++ NotatnikMechanika.Forms/NotatnikMechanika.Forms/MainPage.xaml.cs
@@ -1,11 +1,7 @@
-﻿using System.ComponentModel;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace NotatnikMechanika.Forms
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
