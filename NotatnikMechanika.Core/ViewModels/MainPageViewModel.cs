a5b60d4c7538c48c4913f9ac8d7b02a5f1718e8e
Michał Woźniak <michalwozniak46@gmail.com>
2019-07-04 18:53:12 +0200

    Hamburger menu

--- NotatnikMechanika.Core/ViewModels/MainPageViewModel.cs
+++ NotatnikMechanika.Core/ViewModels/MainPageViewModel.cs
@@ -1,8 +1,25 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using PropertyChanged;
+using System.Windows.Input;
 
 namespace NotatnikMechanika.Core.ViewModels
 {
+    [AddINotifyPropertyChangedInterface]
     public class MainPageViewModel : MvxViewModel
     {
+        public string Text { get; set; } = "elo";
+
+        public ICommand Click { get; set; }
+
+        public MainPageViewModel()
+        {
+            Click = new MvxCommand(ClickEvent);
+        }
+
+        public void ClickEvent()
+        {
+            Text = "Dupa";
+        }
     }
 }
