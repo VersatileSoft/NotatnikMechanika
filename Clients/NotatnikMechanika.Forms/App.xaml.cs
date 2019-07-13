17c4fe50bc4448f6383d648d84078e60e80b85f4
Michał Woźniak <michalwozniak46@gmail.com>
2019-07-13 20:44:28 +0200

    fix

--- Clients/NotatnikMechanika.Forms/App.xaml.cs
+++ Clients/NotatnikMechanika.Forms/App.xaml.cs
@@ -9,19 +9,6 @@ namespace NotatnikMechanika.Forms
         public App()
         {
             InitializeComponent();
-<<<<<<< HEAD
-=======
-
-            switch (Device.RuntimePlatform)
-            {
-                case Device.WPF:
-                    MainPage = new DashboardView();
-                    break;
-                default:
-                    MainPage = new AppShell();
-                    break;
-            }
->>>>>>> 5d5264017b774d93c3f9a2e8ae838480ab931b82
         }
 
         protected override void OnStart()
