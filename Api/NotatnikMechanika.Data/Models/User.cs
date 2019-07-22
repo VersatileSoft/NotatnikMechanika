2ea7155350bce327af7bb3f2944489a1a45b4442
Michał Woźniak <michalwozniak46@gmail.com>
2019-07-22 10:51:25 +0200

    Remove userName.

--- Api/NotatnikMechanika.Data/Models/User.cs
+++ Api/NotatnikMechanika.Data/Models/User.cs
@@ -5,7 +5,6 @@ namespace NotatnikMechanika.Data.Models
     public class User
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
         public string HashedPassword { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
