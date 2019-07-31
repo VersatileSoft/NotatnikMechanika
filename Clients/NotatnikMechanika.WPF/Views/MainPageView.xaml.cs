04586776ff8b37f1a77eb922b1978ba9d6c0bdc6
Michał Woźniak <michalwozniak46@gmail.com>
2019-07-31 17:56:29 +0200

    Resizing window and hiding menu.

--- Clients/NotatnikMechanika.WPF/Views/MainPageView.xaml.cs
+++ Clients/NotatnikMechanika.WPF/Views/MainPageView.xaml.cs
@@ -1,13 +1,45 @@
 ﻿using MvvmCross.Platforms.Wpf.Views;
+using System;
+using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace NotatnikMechanika.WPF.Views
 {
     // [MvxContentPresentation()]
     public partial class MainPageView : MvxWpfView
     {
+
+        private DoubleAnimation _showMenuAnimation;
+        private DoubleAnimation _hideMenuAnimation;
+        private bool _isHide = false;
+
+
         public MainPageView()
         {
             InitializeComponent();
+            _showMenuAnimation = new DoubleAnimation(250, TimeSpan.FromMilliseconds(300));
+            _hideMenuAnimation = new DoubleAnimation(60, TimeSpan.FromMilliseconds(300));
+        }
+
+        private void MvxWpfView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ActualWidth < 600)
+            {
+                if (!_isHide)
+                {
+
+                    Master.BeginAnimation(WidthProperty, _hideMenuAnimation);
+                    _isHide = true;
+                }
+            }
+            else
+            {
+                if (_isHide)
+                {
+                    Master.BeginAnimation(WidthProperty, _showMenuAnimation);
+                    _isHide = false;
+                }
+            }
         }
     }
 }
